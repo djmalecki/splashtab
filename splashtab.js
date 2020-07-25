51679fc0e7f43aa357cf0dafd95ed5d7ce93ecdc
djmalecki <danmalecki2@gmail.com>
2020-07-25 18:42:34 +0100

    by default use window resolution

--- splashtab.js
+++ splashtab.js
@@ -1,39 +1,34 @@
 window.onload = onLoad;
 
 function onLoad() {
-  console.log("page loaded");
-  var randURL = getImage();
-  document.body.style.background = 'url(' + randURL + ') no-repeat center center fixed';
+  var resolution = window.screen.width * window.devicePixelRatio + "x" + window.screen.height * window.devicePixelRatio;
+
+  //var URL = getImage();
+  var URL = "https://source.unsplash.com/random/" + resolution;
+  document.body.style.background = 'url(' + URL + ') no-repeat center center fixed';
 }
 
 
 function getImage() {
   //get user prefs res' from localStorage
   var resString = localStorage.getItem("resPref");
-  console.log("resString " + resString);
 
   //split string into array
   var resArray = resString.split("/");
-  console.log("resArray " + resArray);
 
   //get length of array
   var numberOfRes = resArray.length - 1;
-  console.log("numberOfRes " + numberOfRes);
 
   if (numberOfRes < 1) {
-    console.log("no res chosen, return default");
     return "https://source.unsplash.com/random/1920x1080";
   }
 
   //chose random number based on array length
   var randIndex = randomNumber(numberOfRes);
-  console.log("randIndex " + randIndex);
 
   //use random number as index to select resolution and return address string
   var selectRes = resArray[randIndex];
-  console.log("selectRes " + selectRes);
 
-  console.log("https://source.unsplash.com/random/" + selectRes);
   return "https://source.unsplash.com/random/" + selectRes;
 
 }
